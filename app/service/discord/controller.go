9b5592b9d506a2b8e0860dc96a04a8e076c55fa1
FlameInTheDark <viktorfreedom@gmail.com>
2020-11-13 17:33:40 +0500

    added discord's error and warning messages

--- app/service/discord/controller.go
+++ app/service/discord/controller.go
@@ -1,6 +1,7 @@
 package discord
 
 import (
+	"fmt"
 	"github.com/FlameInTheDark/arcane-service-template/app/service/discord/embed"
 	"github.com/bwmarrin/discordgo"
 	"go.uber.org/zap"
@@ -35,5 +36,21 @@ func (s *Service) SendSimpleEmbed(channel, title, content string) error {
 	s.RLock()
 	defer s.RUnlock()
 	msg := embed.NewEmbed("").Field(title, content, false).Color(0x00ff00).GetMessageSend()
+	return s.SendComplex(channel, msg)
+}
+
+// Send simple embedded error message with title, field title, content and username in footer
+func (s *Service) SendErrorMessage(channel, title, field, content, username string) error {
+	s.RLock()
+	defer s.RUnlock()
+	msg := embed.NewEmbed(title).Field(field, content, false).Color(0xff0000).Footer(fmt.Sprintf("Requested by %s", username)).GetMessageSend()
+	return s.SendComplex(channel, msg)
+}
+
+// Send simple embedded error message with title, field title, content and username in footer
+func (s *Service) SendWarningMessage(channel, title, field, content, username string) error {
+	s.RLock()
+	defer s.RUnlock()
+	msg := embed.NewEmbed(title).Field(field, content, false).Color(0xffff00).Footer(fmt.Sprintf("Requested by %s", username)).GetMessageSend()
 	return s.SendComplex(channel, msg)
 }
